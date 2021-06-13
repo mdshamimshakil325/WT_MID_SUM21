a2e0bf6a1dc830f9fb96aad1c292ff53108e93b3
MD SHAMIM SHAKIL <78806579+mdshamimshakil325@users.noreply.github.com>
2021-06-13 13:34:19 +0600

    LAB-3(php)  80% Completed.

---
+++ LAB-3/Problem-3.php
@@ -0,0 +1,19 @@
+<html>
+	<body>
+		Problem-3
+
+		<?php
+			$length=81;
+			$width=80;
+			$Perimeter=2*($length+$width);
+			$Area=$length*$width;
+			echo"The Perimeter is = $Perimeter<br>";
+			echo"The Area is = $Area<br>";
+			if($length==$width)
+			{
+				echo"the shape is square";
+			}
+			
+		?>
+	</body>	
+</html>
