7599e4b5b8ae82c5e8d94c7e3e7dbbfd11a340e9
MD SHAMIM SHAKIL <78806579+mdshamimshakil325@users.noreply.github.com>
2021-06-22 14:02:34 +0600

    form creation

---
+++ WEBTECH Project Form/Registration.php
@@ -0,0 +1,91 @@
+<html>
+	<body>
+	<fieldset><legend><h1>Hospital Management System</h1></legend>
+	<form border>
+	<table align = "center">
+	<tr>
+	
+	
+	<form>
+	<table border="1">
+		<tr>
+			<th colspan="2"><b>Registration Form</b></th>
+		</tr>
+		<tr>
+			<td>Enter Name</td>
+						<td><input type="text"></td>
+		</tr>
+		
+		<tr>
+		<td>Enter phone</td>
+						<td><input type="Password"></td>
+		</tr>
+		
+		<tr>
+		<td>Enter Email</td>
+						<td><input type="Password"></td>
+		</tr>
+		
+		<tr>
+		<td>Enter Password</td>
+						<td><input type="Password"></td>
+		</tr>
+		
+		<tr>
+		<td>Confirm Password</td>
+						<td><input type="Password"></td>
+		</tr
+		<tr>
+			<td>Enter Address</td>
+						<td><textarea></textarea></td>
+		</tr>
+		<tr>
+			<td>Registration for</td>
+				<td>
+					<input type="checkbox" >Doctor<br>
+					<input type="checkbox" >Nurse<br>
+					<input type="checkbox" >Patient<br>
+					<input type="checkbox" >Customer<br>
+							
+				</td>
+		</tr>
+		<tr>
+			<td>Gender</td>
+				<td>
+				<input type="radio" name="Gender"> Male <input name="Gender" type="radio"> Female
+				</td>
+		</tr>
+		<tr>
+			<td>
+				Select ur age
+				<td>
+					<select>
+						<option>18+</option>
+						<option>22+</option>
+						<option>26+</option>
+						<option>30+</option>
+						<option>40</option>
+					</select> 
+				</td>
+			</td>
+		</tr>
+		
+		<tr>
+			<td>Agree term and conditions</td>
+				<td>
+				<input type="radio" name="agree"> YES <input name="agree" type="radio"> NO
+				</td>
+		</tr>
+		
+		
+		<tr>
+		<td align="center" colspan="2">
+			<input type="submit" value="Register">
+			<input type="submit" value="Reset">
+			<input type="submit" value="Already Registred?/login here">
+		</td>
+		</tr>
+	</table>
+	</form>
+	</body>
+</html>
---
+++ WEBTECH Project Form/login/login2.php
@@ -0,0 +1,53 @@
+<html>
+	<body>
+	
+	<table   align="center" border="5">
+
+                <tr>
+                    <th colspan="3">
+                       <h1>Hospital Management System</h1> 
+                    </th>
+                </tr>
+	
+	
+	<tr>
+		<td>
+		Username
+		</td>
+		<td>
+			<input type="text">
+		</td>
+	</tr>
+	<tr>
+		<td>
+		Password
+		</td>
+		<td>
+			<input type="password">
+		</td>
+	</tr>
+	
+	<tr>
+		<td colspan="2" align="center">
+			<input type="submit" value="Forget password">
+		</td>
+	</tr>
+	
+	
+	
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<input type="submit" value="Log In">
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<input type="submit" value="Register New user">
+		</td>
+	</tr>
+	</table>	
+	<form>
+	</fieldset>
+	</body>
+</html>
